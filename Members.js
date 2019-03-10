f1f560a104cf254d8d51295e752e3035c5d399d0
Warren D. Rezane <warzkie123@gmail.com>
2019-03-10 22:34:07 +0800

    Update Members.js

--- Members.js
+++ Members.js
@@ -1,28 +1,3 @@
-const members = [
-    // {
-    //     id: 1,
-    //     name: 'Warren D. Rezane',
-    //     email: 'warzkie123@gmail.com',
-    //     status: 'active'
-    // },
-    // {
-    //     id: 2,
-    //     name: 'Warren D. Rezane',
-    //     email: 'warzkie123@gmail.com',
-    //     status: 'active'
-    // },
-    // {
-    //     id: 3,
-    //     name: 'Warren D. Rezane',
-    //     email: 'warzkie123@gmail.com',
-    //     status: 'active'
-    // },
-    // {
-    //     id: 4,
-    //     name: 'Warren D. Rezane',
-    //     email: 'warzkie123@gmail.com',
-    //     status: 'active'
-    // }
-]
+const members = []
 
 module.exports = members
